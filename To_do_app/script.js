b7fe2b41121af4271b7fea55c36220d309a224d7
theSumit <thesumit0x@gmail.com>
2025-01-23 23:39:20 +0530

    Added some javaScript for more functionality.

--- To_do_app/script.js
+++ To_do_app/script.js
@@ -18,35 +18,40 @@ deleteBtn.classList.add("delete");
 editBtn.innerText='Edit'
 deleteBtn.innerText='Delete'
    
- editBtn.addEventListener('click',(e)=>{S
+ editBtn.addEventListener('click',(e)=>{
      taskDiv.classList.add('hide')
    if(e){
     inputTag.value=spanTag.innerText
    inputTag.focus()
     taskDiv.remove()
    }
-   
-   if(!tasksListContainer.children){
-    console.log('empty');
+   if(tasksListContainer.innerText === ''){
+    tasksListContainer.classList.add('hide')
   }
  })
 
+ deleteBtn.addEventListener('click',(e)=>{
+  alert('Are you sure? you want to delete your task')
+  taskDiv.remove()
+  if(tasksListContainer.innerText === ''){
+    tasksListContainer.classList.add('hide')
+  }
+ })
 
   if (inputTag.value) {
     tasksListContainer.classList.remove("hide");
     spanTag.innerText = inputTag.value;
-    console.log(spanTag.innerText);
     btnsContainer.append(editBtn, deleteBtn);
     taskDiv.append(spanTag, btnsContainer);
     tasksListContainer.append(taskDiv);
+        inputTag.value =''
   } else{
     alert('Write something in inputField:')
   }
+
+  if(tasksListContainer === ''){
+
+  }
 };
-
-
-
-
-
 
 addButton.addEventListener("click", () => addTasks());
